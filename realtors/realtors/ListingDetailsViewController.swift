d87d42910bdda92725ee4bb3aaeb93f1c37ce212
Suresh Balla <suresh.balla@outlook.com>
2016-03-23 17:30:28 +0530

    Added root navigation controller for app

---
+++ realtors/realtors/ListingDetailsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ListingDetailsViewController.swift
+//  realtors
+//
+//  Created by Suresh.Balla on 23/03/16.
+//  Copyright © 2016 sureshballa. All rights reserved.
+//
+
+import UIKit
+
+class ListingDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
